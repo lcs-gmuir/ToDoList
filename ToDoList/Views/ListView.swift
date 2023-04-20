9c81fa548091ed6dff846b220ab5ece2ad6e5f4c
griffin muir <gmuir@lcs.on.ca>
2023-04-20 09:00:35 -0400

    Finished search bar and fixed many syntax errors.

--- ToDoList/Views/ListView.swift
+++ ToDoList/Views/ListView.swift
@@ -1,54 +1,61 @@
+//
+//  ListView.swift
+//  ToDoList
+//
+//  Created by griffin muir on 2023-04-03.
+//
+
 import SwiftUI
 import Blackbird
 
 struct ListView: View {
+    
+    //MARK: Stored property
+    @Environment(\.blackbirdDatabase) var db: Blackbird.Database?
+    
+    
+    @State var newItemDescription: String = ""
+    @State var searchText: String = ""
+    //MARK: Computed Property
+    var body: some View {
+        NavigationView{
+            Group{
+                VStack{
+                    
+                    HStack{
+                        TextField("Enter a to-do item", text: $newItemDescription)
+                            .padding()
+                        Button(action: {
+                            Task{
+                                try await db!.transaction { core in
+                                    try core.query("INSERT INTO TodoItem (description) VALUES (?)", newItemDescription)
+                                    
+                                }
+                                newItemDescription = ""
+                            }
+                        }, label:{
+                            Text("ADD")
+                                .font(.caption)
+                            
+                        })
+                        .padding()
+                    }
+                    
+                    ListItemsView(filteredOn: searchText)
+                    .searchable(text: $searchText)
+                    
+                }
+            }
+            .navigationTitle("To do")
 
-//MARK: Stored property
-@Environment(\.blackbirdDatabase) var db: Blackbird.Database?
-
-
-@State var newItemDescription: String = ""
-@State var searchText: String = ""
-//MARK: Computed Property
-var body: some View {
-NavigationView{
-Group{
-VStack{
-
-HStack{
-TextField("Enter a to-do item", text: $newItemDescription)
-.padding()
-Button(action: {
-Task{
-try await db!.transaction { core in
-try core.query("INSERT INTO TodoItem (description) VALUES (?)", newItemDescription)
-
-}
-newItemDescription = ""
-}
-}, label:{
-Text("ADD")
-.font(.caption)
-
-})
-.padding()
-}
-
-ListItemsView(filteredOn: searchText)
-.searchable(text: $searchText)
-
-}
-}
-.navigationTitle("To do")
-
-}
-}
-
+        }
+    }
+   
 }
 
 struct ListView_Previews: PreviewProvider {
-static var previews: some View {
-ListView()
-.environment(\.blackbirdDatabase, AppDatabase.instance)
-}
+    static var previews: some View {
+            ListView()
+            .environment(\.blackbirdDatabase, AppDatabase.instance)
+    }
 }
--- ToDoList/Views/listItemView.swift
+++ ToDoList/Views/listItemView.swift
@@ -1,74 +1,80 @@
+//
+//  ListItemsView.swift
+//  ToDoList
+//
+//  Created by griffin muir on 2023-04-13.
+//
 import Blackbird
 import SwiftUI
 
 struct ListItemsView: View {
-
-//MARK: STORED PROPERTIES
-
-@Environment(\.blackbirdDatabase) var db: Blackbird.Database?
-
-@BlackbirdLiveModels var todoItems: Blackbird.LiveResults<TodoItem>
-
-//MARK: COMPUTED PROPERTIES
-
-var body: some View {
-List{
-ForEach(todoItems.results){ currentItem in
-
-Label(title: {
-Text(currentItem.description)
-}, icon: {
-if currentItem.completed == true {
-Image(systemName: "checkmark.circle")
-}else{
-Image(systemName: "circle")
-}
-})
-.onTapGesture {
-Task {
-try await db!.transaction { core in
-try core.query("UPDATE TodoItem SET completed = (?) WHERE id = (?)", !currentItem.completed, http://currentitem.id/)
-}
-}
-}
-
-}
-.onDelete(perform: removeRows)
-}
-}
-
-//MARK: INITIALIZERS
-
-init(filteredOn searchText: String){
-_todoItems = BlackbirdLiveModels({ db in
-try await http://todoitem.read/(from: db,
-sqlWhere: "description LIKE ?", "%\(searchText)%")
-})
-
-}
-
-//MARK: Functions
-func removeRows(at offsets: IndexSet){
-Task{
-try await db!.transaction{ core in
-var idList = ""
-for offset in offsets{
-idList += "\(todoItems.results[offset].id),"
-}
-print(idList)
-idList.removeLast()
-print(idList)
-
-try core.query("DELETE FROM TodoItem WHERE id IN (?)",idList)
-}
-}
-
-}
+    
+    //MARK: STORED PROPERTIES
+    
+    @Environment(\.blackbirdDatabase) var db: Blackbird.Database?
+    
+    @BlackbirdLiveModels var todoItems: Blackbird.LiveResults<TodoItem>
+    
+    //MARK: COMPUTED PROPERTIES
+    
+    var body: some View {
+        List{
+            ForEach(todoItems.results){ currentItem in
+                
+                Label(title: {
+                    Text(currentItem.description)
+                }, icon: {
+                    if currentItem.completed == true {
+                        Image(systemName: "checkmark.circle")
+                    }else{
+                        Image(systemName: "circle")
+                    }
+                })
+                .onTapGesture {
+                    Task {
+                        try await db!.transaction { core in
+                            try core.query("UPDATE TodoItem SET completed = (?) WHERE id = (?)", !currentItem.completed, currentItem.id)
+                        }
+                    }
+                }
+              
+            }
+            .onDelete(perform: removeRows)
+        }
+    }
+    
+    //MARK: INITIALIZERS
+    
+    init(filteredOn searchText: String){
+        _todoItems = BlackbirdLiveModels({ db in
+            try await TodoItem.read(from: db,
+                                    sqlWhere: "description LIKE ?", "%\(searchText)%")
+        })
+        
+    }
+    
+    //MARK: Functions
+    func removeRows(at offsets: IndexSet){
+        Task{
+            try await db!.transaction{ core in
+                var idList = ""
+                for offset in offsets{
+                    idList += "\(todoItems.results[offset].id),"
+                }
+                print(idList)
+                idList.removeLast()
+                print(idList)
+                
+                try core.query("DELETE FROM TodoItem WHERE id IN (?)",idList)
+            }
+        }
+        
+    }
 }
 
 struct ListItemsView_Previews: PreviewProvider {
-static var previews: some View {
-ListItemsView(filteredOn: "")
-.environment(\.blackbirdDatabase, AppDatabase.instance)
-}
+    static var previews: some View {
+        ListItemsView(filteredOn: "")
+            .environment(\.blackbirdDatabase, AppDatabase.instance)
+    }
 }
